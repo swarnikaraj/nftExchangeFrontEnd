b406322e4a6666fedccd0f99bf5fe7b9334e5a7b
Neer <neer@Neers-MacBook-Pro.local>
2022-04-22 01:23:40 +0530

    token uri

--- nft_exchange_admin/src/App.js
+++ nft_exchange_admin/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 import { useEffect, useState } from "react";
@@ -9,41 +8,34 @@ function App() {
   let obj = {};
 
   function parseall(arr) {
-    for (let j = 0; j < arr.length; j++) {
-      att = JSON.parse(arr[j].tokenURI).attributes;
-
+    for (let j = 0; j < arr.nft.length; j++) {
+      att = JSON.parse(arr.nft[j].tokenURI).attributes;
       for (let i = 0; i < att.length; i++) {
-
-        if ( obj[ att[i]["trait_type"]  ] === undefined) {
-              var t=new Array()
-          obj[att[i]["trait_type"]] = t;
-             
-          t.push(att[i]["value"])
-
-        } else {
-
-          
-                
-          obj[att[i]["trait_type"]].push(att[i]["trait_type"]);
+        var traits = att[i].trait_type
+        if (obj[traits] === undefined) {
+          obj[traits] = []
+        }
+        var found = (obj[traits]).includes(att[i].value);
+        if (!found) {
+          obj[traits].push(att[i].value)
         }
       }
     }
+    console.log(obj)
+
 
     setArr1(arr);
   }
 
   useEffect(() => {
     fetch(
-      "http://127.0.0.1:1234/nft/single/0x42069ABFE407C60cf4ae4112bEDEaD391dBa1cdB"
+      "http://localhost:1234/nft/all/0x42069ABFE407C60cf4ae4112bEDEaD391dBa1cdB"
     )
       .then((res) => res.json())
       .then((res) => {
-        // console.log(JSON.parse(res[0].tokenURI).attributes);
-        setArr1(res);
+        // console.log(JSON.parse(res.nft[0].tokenURI).attributes);
+        parseall(res);
       });
-
-    parseall(arr1);
-    console.log(obj);
   }, []);
 
   return <div className="App"></div>;
