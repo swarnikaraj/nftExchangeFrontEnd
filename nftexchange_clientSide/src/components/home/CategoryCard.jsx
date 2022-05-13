c5ef013a4a3588d4b525ca3924c590cdd4339f30
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2022-05-13 12:22:39 +0530

    browse by category cards

--- nftexchange_clientSide/src/components/home/CategoryCard.jsx
+++ nftexchange_clientSide/src/components/home/CategoryCard.jsx
@@ -1,9 +1,81 @@
 import "../../styles/common.css";
-
+import React from "react";
 export const CatCard = () => {
+  const cat = [
+    { name: "Art", img: "https://opensea.io/static/images/categories/art.png" },
+    {
+      name: "Collectibles",
+      img: "https://opensea.io/static/images/categories/collectibles.png",
+    },
+    {
+      name: "Domain Name",
+      img: "https://opensea.io/static/images/categories/domain-names.png",
+    },
+    {
+      name: "Music",
+      img: "https://opensea.io/static/images/categories/music.png",
+    },
+    {
+      name: "Photography",
+      img: "https://opensea.io/static/images/categories/photography-category.png",
+    },
+    {
+      name: "Sports",
+      img: "https://opensea.io/static/images/categories/sports.png",
+    },
+    {
+      name: "Trading Cards",
+      img: "https://opensea.io/static/images/categories/trading-cards.png",
+    },
+    {
+      name: "Utility",
+      img: "https://opensea.io/static/images/categories/utility.png",
+    },
+    {
+      name: "Virtual Cards",
+      img: "https://opensea.io/static/images/categories/virtual-worlds.png",
+    },
+  ];
   return (
     <>
-      <div className=""></div>
+      <div>
+        <div className="container flex justify-center mx-auto pt-16">
+          <div>
+            <p className="text-gray-200 font-extrabold text-3xl text-center  pb-3">
+              Browse By Category
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full  px-10 pt-10">
+          <div className="container mx-auto">
+            <div className="lg:flex md:flex sm:flex items-center xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around">
+              {cat.map((item) => (
+                <div className="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
+                  <div className="rounded overflow-hidden shadow-md bg-[#655D8A]">
+                    <div className="absolute -mt-20 w-full flex justify-center">
+                      <div className="h-32 w-25">
+                        <img
+                          src={item.img}
+                          alt
+                          className="rounded-full object-cover h-full w-full shadow-md"
+                        />
+                      </div>
+                    </div>
+                    <div className="px-4 mt-16 py-4">
+                      <p className="text-gray-200 text-sm text-center">
+                        <a href="javascript:void(0)" className="">
+                          <div>{item.name}</div>
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- nftexchange_clientSide/src/components/home/infoBanner.jsx
+++ nftexchange_clientSide/src/components/home/infoBanner.jsx
@@ -3,7 +3,81 @@ import "../../styles/common.css";
 export const InfoBanner = () => {
   return (
     <>
-      <div className=""></div>
+      <div classname="overflow-y-hidden bg-gray-100">
+        <div className="mx-auto container f-f-p px-4 xl:px-0 py-24">
+          <h1 className="focus:outline-none text-center text-3xl lg:text-4xl font-extrabold lg:leading-9 tracking-wider text-gray-200">
+            How it works?
+          </h1>
+          <div className="md:mt-24 f-f-p">
+            <div className=" md:flex justify-center w-full">
+              <div className="flex flex-col items-center md:items-end md:pr-12 md:border-r-4 border-gray-300 relative md:w-1/2">
+                <div aria-label="Create a Wallet" role="img">
+                  <img
+                    className="focus:outline-none mt-10"
+                    src="https://tuk-cdn.s3.amazonaws.com/can-uploader/3_step_center_aligned_how_it_worksSvg5.svg"
+                    alt="how it work"
+                  />
+                </div>
+
+                <div className="flex mt-12 flex-col items-center lg:items-end md:w-8/12">
+                  <h1 className="focus:outline-none text-xl font-bold leading-5">
+                    Add Your Nfts
+                  </h1>
+                  <h2 className="focus:outline-none text-gray-500 mt-3 pl-3 text-center md:text-right text-base leading-6 tracking-wide">
+                    Upload your work (image, video, audio, or 3D art), add a
+                    title and description, and customize your NFTs with
+                    properties, stats, and unlockable content..{" "}
+                  </h2>
+                </div>
+                <div aria-label="transactions" role="img">
+                  <img
+                    className="focus:outline-none mt-24"
+                    src="https://tuk-cdn.s3.amazonaws.com/can-uploader/3_step_center_aligned_how_it_worksSvg3.svg"
+                    alt
+                  />
+                </div>
+
+                <img
+                  className="hidden md:block absolute right-0 top-0 -mt-2 -mr-1"
+                  src="https://cdn.tuk.dev/assets/components/111220/Fs7/line.png"
+                  alt
+                />
+              </div>
+
+              <div className="flex flex-col items-center md:items-start md:pl-12 lg:border-gray-400 mt-20 md:mt-0 md:w-1/2">
+                <div className="flex mt-6 flex-col items-center md:items-start md:w-8/12">
+                  <h1 className="focus:outline-none text-xl font-bold leading-5">
+                    Create a Wallet
+                  </h1>
+                  <h2 className="focus:outline-none text-gray-500 mt-3 text-base leading-6 tracking-wide">
+                    Once you’ve set up your wallet of choice, connect it to
+                    OpenSea by clicking the wallet icon in the top right corner.{" "}
+                  </h2>
+                </div>
+
+                <div aria-label="wallet" role="img">
+                  <img
+                    className="focus:outline-none mt-32"
+                    src="https://tuk-cdn.s3.amazonaws.com/can-uploader/3_step_center_aligned_how_it_worksSvg5.svg"
+                    alt
+                  />
+                </div>
+
+                <div className="flex mt-6 flex-col items-center md:items-start md:w-8/12">
+                  <h1 className="focus:outline-none text-xl font-bold leading-5">
+                    List them for Sale
+                  </h1>
+                  <h2 className="focus:outline-none text-gray-500 mt-3 text-base leading-6 tracking-wide">
+                    Choose between auctions, fixed-price listings, and
+                    declining-price listings. You choose how you want to sell
+                    your NFTs, and we help you sell them!
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
