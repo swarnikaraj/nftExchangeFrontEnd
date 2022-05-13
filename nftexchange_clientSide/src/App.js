78ac5b9ee8228369dde12b8ad589ca0a91fe1e7b
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2022-05-13 10:55:31 +0530

    footer

--- nftexchange_clientSide/src/App.js
+++ nftexchange_clientSide/src/App.js
@@ -2,20 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { Nav } from "./components/navbar/navbar";
-import {Router} from "./routes/router"
+import { Footer } from "./components/footer/footer";
+import { Router } from "./routes/router";
 
 function App() {
   return (
     <div className="App">
-      
-      
-          <Nav />
+      <Nav />
 
-         <Router/>
+      <Router />
 
-         
-       
-
+      <Footer />
     </div>
   );
 }
